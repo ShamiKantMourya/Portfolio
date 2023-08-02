81f72e7baf30ec1be381db57ebececed20cb9492
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-02 23:53:52 +0530

    skill image js

---
+++ src/assests/Skills.js
@@ -0,0 +1,12 @@
+const Skill = {
+    html: require('./html.png'),
+    css: require('./css.png'),
+    javascript: require('./javascript.png'),
+    react: require('./react.png'),
+    tailwind: require('./tailwind.png'),
+    node: require('./node.png'),
+    mongoDb: require('./mongodb.png'),
+    express:require('./express.png')
+}
+
+export default Skill
