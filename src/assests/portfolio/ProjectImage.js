273456e001b26c5ac1d740e952ae41255a7b58db
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-02 23:54:05 +0530

    project image js file

---
+++ src/assests/portfolio/ProjectImage.js
@@ -0,0 +1,9 @@
+const ProjectImage = {
+    eCommerce: require('./e-commerce.jpg'),
+    socialMedia: require('./reactParallax.jpg'),
+    videoLibrary: require('./arrayDestruct.jpg'),
+    recipeSite: require('./usestate.jpg'),
+    habitTracker: require('./installNode.jpg'),
+};
+
+export default ProjectImage;
