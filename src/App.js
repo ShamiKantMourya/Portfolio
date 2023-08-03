f3782dc594e5c73b722aad344e6353eb62084118
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-03 18:36:37 +0530

    remove classname

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import About from './Components/About';
+import Contact from './Components/Contact';
 import HomePage from './Components/HomePage';
 import Navbar from './Components/Navbar';
 import Project from './Components/Project';
@@ -8,12 +9,13 @@ import SocialIcons from './Components/SocialIcons';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <HomePage />
       <About />
       <Project />
       <Skills />
+      <Contact />
       <SocialIcons />
     </div>
   );
