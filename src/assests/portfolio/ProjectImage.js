43da4a353b1235060723ed1ae61344921472b238
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-14 13:11:31 +0530

    portfolio img

--- src/assests/portfolio/ProjectImage.js
+++ src/assests/portfolio/ProjectImage.js
@@ -1,8 +1,8 @@
 const ProjectImage = {
     eCommerce: require('./e-commerce.jpg'),
     socialMedia: require('./reactParallax.jpg'),
-    videoLibrary: require('./arrayDestruct.jpg'),
-    recipeSite: require('./usestate.jpg'),
+    videoLibrary: require('./videolibrary.PNG'),
+    recipeSite: require('./recipesite.JPG'),
     habitTracker: require('./installNode.jpg'),
 };
 
